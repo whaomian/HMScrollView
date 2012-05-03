cf10cffe6f3b7dab9e07cbe8e44f0122b1301381
Eric Wang <ericwang@EricmatoMacBook-Pro.local>
2012-05-03 12:23:52 +0800

    Initial Commit

---
+++ HMScrollViewDemo/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  HMScrollViewDemo
+//
+//  Created by Wang Eric on 12-5-3.
+//  Copyright (c) 2012年 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
